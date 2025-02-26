24987b5d0be01ed8e23613c4630d7247c2485ad9
Thakar-Devam <devamthakar.dev@gmail.com>
2025-02-26 11:40:36 +0530

    ADD ASGI file

---
+++ backend/asgi.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_asgi import ASGIApp  # Converts Flask (WSGI) to ASGI
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Flask ASGI!"
+
+# Convert Flask to ASGI
+asgi_app = ASGIApp(app)
+
+if __name__ == "__main__":
+    app.run()

32ecc3bb3f1b1d7b334ed4ee39fe60554df0fb27
Thakar-Devam <devamthakar.dev@gmail.com>
2025-02-26 15:39:29 +0530

    modify

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ CORS(app, supports_credentials=True, origins=[
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
     return response
 
 # Sample route

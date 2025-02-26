8ef50a118508a2f4a2c36f17c4b9e5022886e881
Thakar-Devam <devamthakar.dev@gmail.com>
2025-02-26 12:15:49 +0530

    option 1 added

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,14 +1,7 @@
-from flask import Flask
-from flask_asgi import ASGIApp  # Converts Flask to ASGI
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def home():
-    return "Hello from Flask ASGI!"
-
-# Convert Flask to ASGI
-asgi_app = ASGIApp(app)
-
-if __name__ == "__main__":
-    app.run()
+@app.get("/")
+async def home():
+    return {"message": "Hello from FastAPI!"}
